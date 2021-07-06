5950e2b3419837d0648a52dd44fafd847198cd64
JeongHun0612 <sjh960612@naver.com>
2021-07-06 17:44:53 +0900

    operator result number update

--- CalucatorProject/MainWindow.xaml.cs
+++ CalucatorProject/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CalucatorProject
 {
@@ -20,11 +9,12 @@ namespace CalucatorProject
     /// </summary>
     public partial class MainWindow : Window
     {
-        private double firstNumber = 0;
-        private double secondNumber = 0;
-        private double resultNumber = 0;
-        private String selectedOperator = null;
-        private String progressResultLabelContent = null;
+        private double firstNumber = 0.0;
+        private double secondNumber = 0.0;
+        private double resultNumber = 0.0;
+        private string selectedOperator = null;
+        private string progressResultLabelContent = null;
+
         private bool isCalu = false;
 
         public MainWindow()
@@ -46,14 +36,17 @@ namespace CalucatorProject
             {
                 return n1 + n2;
             }
+
             public static double Subtract(double n1, double n2)
             {
                 return n1 - n2;
             }
+
             public static double Multiple(double n1, double n2)
             {
                 return n1 * n2;
             }
+
             public static double Divide(double n1, double n2)
             {
                 return n1 / n2;
@@ -64,7 +57,6 @@ namespace CalucatorProject
         {
             Button[] numBtnArray = { zeroBtn, oneBtn, twoBtn, threeBtn, fourBtn, fiveBtn, sixBtn, sevenBtn, eightBtn, nightBtn };
             int selectedValue = 0;
-            //if(selectedOperator != null) resultLabel.Content = "0";
 
             for (int i = 0; i < numBtnArray.Length; i++)
             {
@@ -75,65 +67,76 @@ namespace CalucatorProject
                     resultLabel.Content = progressResultLabelContent;
                 }
             }
+
+            isCalu = false;
         }
 
         private void operationBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (sender == plusBtn)
+            void test(string m_operator)
             {
-                if (selectedOperator != null)
+                if (!isCalu)
                 {
-                    secondNumber = int.Parse(resultLabel.Content.ToString());
-                    resultPreviewLabel.Content += resultLabel.Content.ToString() + selectedOperator;
+                    isCalu = true;
 
-                    switch (selectedOperator)
+                    if (resultPreviewLabel.Content == null)
                     {
-                        case "+":
-                            {
-                                resultLabel.Content = SimpleMath.Add(firstNumber, secondNumber);
-                                firstNumber = SimpleMath.Add(firstNumber, secondNumber);
-                                break;
-                            }
-                        case "-":
-                            {
-                                resultLabel.Content = SimpleMath.Add(firstNumber, secondNumber);
-                                break;
-                            }
-                        case "*":
-                            {
-                                resultLabel.Content = SimpleMath.Add(firstNumber, secondNumber);
-                                break;
-                            }
-                        case "/":
-                            {
-                                resultLabel.Content = SimpleMath.Add(firstNumber, secondNumber);
-                                break;
-                            }
-                        default:
-                            {
-                                break;
-                            }
+                        double.TryParse(resultLabel.Content.ToString(), out firstNumber);
                     }
+                    else
+                    {
+                        double.TryParse(resultLabel.Content.ToString(), out secondNumber);
+
+                        string lastString = resultPreviewLabel.Content.ToString().Substring(resultPreviewLabel.Content.ToString().Length - 1);
+
+                        switch (lastString)
+                        {
+                            case "+":
+                                resultNumber = SimpleMath.Add(firstNumber, secondNumber);
+                                break;
+                            case "-":
+                                resultNumber = SimpleMath.Subtract(firstNumber, secondNumber);
+                                break;
+                            case "*":
+                                resultNumber = SimpleMath.Multiple(firstNumber, secondNumber);
+                                break;
+                            case "/":
+                                resultNumber = SimpleMath.Divide(firstNumber, secondNumber);
+                                break;
+                        }
+                        resultLabel.Content = resultNumber;
+                        firstNumber = resultNumber;
+                    }
+                    resultPreviewLabel.Content += progressResultLabelContent + m_operator;
+                    progressResultLabelContent = null;
                 }
                 else
                 {
-                    selectedOperator = "+";
-                    firstNumber = int.Parse(resultLabel.Content.ToString());
-                    resultPreviewLabel.Content += resultLabel.Content.ToString() + selectedOperator;
+                    resultPreviewLabel.Content = resultPreviewLabel.Content.ToString().Substring(0, resultPreviewLabel.Content.ToString().Length - 1) + m_operator;
                 }
-                progressResultLabelContent = null;
             }
+
+
+            if (sender == plusBtn)
+            {
+                selectedOperator = "+";
+                test(selectedOperator);
+            }
+
             if (sender == minusBtn)
             {
                 selectedOperator = "-";
+                test(selectedOperator);
             }
             if (sender == multBtn)
             {
                 selectedOperator = "*";
+                test(selectedOperator);
             }
             if (sender == divideBtn)
             {
                 selectedOperator = "/";
+                test(selectedOperator);
             }
 
         }
@@ -154,6 +157,27 @@ namespace CalucatorProject
 
         private void resultBtn_Click(object sender, RoutedEventArgs e)
         {
+            double.TryParse(resultLabel.Content.ToString(), out secondNumber);
+
+            string lastString = resultPreviewLabel.Content.ToString().Substring(resultPreviewLabel.Content.ToString().Length - 1);
+
+            switch (lastString)
+            {
+                case "+":
+                    resultNumber = SimpleMath.Add(firstNumber, secondNumber);
+                    break;
+                case "-":
+                    resultNumber = SimpleMath.Subtract(firstNumber, secondNumber);
+                    break;
+                case "*":
+                    resultNumber = SimpleMath.Multiple(firstNumber, secondNumber);
+                    break;
+                case "/":
+                    resultNumber = SimpleMath.Divide(firstNumber, secondNumber);
+                    break;
+            }
+            resultLabel.Content = resultNumber;
+            resultPreviewLabel.Content = null;
         }
 
         private void dotBtn_Click(object sender, RoutedEventArgs e)
